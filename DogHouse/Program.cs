75b293df08b0be0e4d683c9ae61ba426e657253d
Ellie Beeler <ellie.beeler@hotmail.com>
2021-11-07 18:29:14 -0800

    first commit

---
+++ DogHouse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogHouse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!")
+
+
+        }
+    }
+}
