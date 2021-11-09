aabcbd016ef8a0b4e9dd2022c6177a2b5eef7d21
Ellie Beeler <ellie.beeler@hotmail.com>
2021-11-09 10:48:37 -0800

    Added MainMenu class for loop

---
+++ DogHouse/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHouse
+{
+    class MainMenu
+    {
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to my app");
+            Console.WriteLine("");
+            Console.WriteLine("1. Adoptable Dogs");
+            Console.WriteLine("2. Adoptable Cats");
+            Console.WriteLine("3. Quit");
+            Console.WriteLine("Please select an option from above");
+
+            string userselection;
+            userselection = Console.ReadLine();
+
+        }
+    }
+}
